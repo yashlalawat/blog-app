7a0c3e73cc1ed0bae9f3278b35e4e7f49928bca1
yashlalawat <yashlalawatyl@gmail.com>
2026-09-05 17:07:36 +0530

    Fix CORS headers

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ const app = express();
 
 app.use(cors({
     origin: 'https://blog-frontend-ae2r.onrender.com',
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(bodyParser.json({ extended: true }));
