e22ba39bd3c63bb455356de10e00487108f4dfca
yashlalawat <yashlalawatyl@gmail.com>
2026-09-05 14:19:15 +0530

    Configure server for Render

--- server/index.js
+++ server/index.js
@@ -3,10 +3,9 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
-//components
+// components
 import Connection from './database/db.js';
 import Router from './routes/route.js';
-
 
 dotenv.config();
 
@@ -17,11 +16,12 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
-
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 Connection(username, password);
 
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running successfully on PORT ${PORT}`);
+});
